67a6d5ded309b6a20f87b63e9e9f5857450b10cc
simonksimon <113455453+simonksimon@users.noreply.github.com>
2023-11-19 12:42:30 +0100

    Delete Adresáre.py

--- Adresáre.py
+++
@@ -1,8 +0,0 @@
-import os
-def files(directory, level=0):
-    for item in os.listdir(directory):
-        if os.path.isdir(directory+"\\"+item) and item[0] not in ".$":
-            if len(os.listdir(directory+"\\"+item))!=0: #vrati aj subory ktore su v danom adresari treba len adresare nie subory
-                print("-",' '*level,item)
-                files(directory+"\\"+item,level+1)
-files(r"C:\Program Files")
