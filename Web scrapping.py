18cd9d06a496850c0f6dd24e7bc682cac6fb85e7
simonksimon <113455453+simonksimon@users.noreply.github.com>
2023-11-27 14:16:25 +0100

    Add files via upload

---
+++ Web scrapping.py
@@ -0,0 +1,131 @@
+import requests
+import urllib
+import urllib.request
+from urllib.request import urlretrieve
+from urllib.request import urlopen, Request
+url = "https://protonvpn.com/free-vpn"
+req1 = requests.get(url)
+#f=(open"bordel.txt" with )
+#    write
+
+bordel=("use strict;(self.webpackChunkprotonvpn_com = self.webpackChunkprotonvpn_com || []).push([[21], {8021: function(e, t, n) {n.r(t),n.d(t, {renderImage")
+bordel=(bordel+"ToString: function() {return b)},swapPlaceholderImage: function() {return g}});var o = n(73238), a = n(52983), r = n(66371);n(20097);let i;")
+bordel=(bordel+"const c = new WeakMap, l = navigator.connection || navigator.mozConnection || navigator.webkitConnection, s = [image, loading, isLoading,")
+bordel=(bordel+"isLoaded, imgClassName, imgStyle, objectPosition, backgroundColor, objectFit];function u(e, t) {e.style.opacity = 1,t && (t.style.opacity =")
+bordel=(bordel+"0)}function d(e, t, n, o, a, r) {const i = e.querySelector([data-main-image]), c = e.querySelector("[data-placeholder-image]"), l = n.has(t);")
+bordel=(bordel+"function s(e) {this.removeEventListener(load, s);const t = e.currentTarget
+                  , n = new Image;
+                n.src = t.currentSrc,
+                n.decode ? n.decode().then((()=>{
+                    u(this, c),
+                    null == a || a({
+                        wasCached: l
+                    })
+                }
+                )).catch((e=>{
+                    u(this, c),
+                    null == r || r(e)
+                }
+                )) : (u(this, c),
+                null == a || a({
+                    wasCached: l
+                }))
+            }
+            return i.addEventListener("load", s),
+            null == o || o({
+                wasCached: l
+            }),
+            Array.from(i.parentElement.children).forEach((e=>{
+                const t = e.getAttribute("data-src")
+                  , n = e.getAttribute("data-srcset");
+                t && (e.removeAttribute("data-src"),
+                e.setAttribute("src", t)),
+                n && (e.removeAttribute("data-srcset"),
+                e.setAttribute("srcset", n))
+            }
+            )),
+            n.add(t),
+            i.complete && s.call(i, {
+                currentTarget: i
+            }),
+            ()=>{
+                i && i.removeEventListener("load", s)
+            }
+        }
+        function g(e, t, a, r, s, u, g) {
+            if (!(0,
+            o.h)()) {
+                let o;
+                const v = (h = ()=>{
+                    o = d(e, t, a, s, u, g)
+                }
+                ,
+                "IntersectionObserver"in window ? (i || (i = new IntersectionObserver((e=>{
+                    e.forEach((e=>{
+                        var t;
+                        e.isIntersecting && (null == (t = c.get(e.target)) || t(),
+                        c.delete(e.target))
+                    }
+                    ))
+                }
+                ),{
+                    rootMargin: "4g" !== (null == l ? void 0 : l.effectiveType) || null != l && l.saveData ? "2500px" : "1250px"
+                })),
+                function(e) {
+                    return c.set(e, h),
+                    i.observe(e),
+                    function() {
+                        i && e && (c.delete(e),
+                        i.unobserve(e))
+                    }
+                }
+                ) : function() {
+                    return h(),
+                    function() {}
+                }
+                )
+                  , f = v(e);
+                var b, m;
+                return "objectFit"in document.documentElement.style || (e.dataset.objectFit = null != (b = r.objectFit) ? b : "cover",
+                e.dataset.objectPosition = "" + (null != (m = r.objectPosition) ? m : "50% 50%"),
+                async function(e) {
+                    "objectFitPolyfill"in window || await n.e(252).then(n.t.bind(n, 252, 23)),
+                    window.objectFitPolyfill(e)
+                }(e)),
+                ()=>{
+                    o && o(),
+                    f()
+                }
+            }
+            var h;
+            return d(e, t, a, s, u, g)
+        }
+        function b(e) {
+            let {image: t, loading: n="lazy", isLoading: i, isLoaded: c, imgClassName: l, imgStyle: u={}, objectPosition: d, backgroundColor: g, objectFit: b="cover"} = e
+              , m = (0,
+            o._)(e, s);
+            const {width: h, height: v, layout: f, images: w, placeholder: p, backgroundColor: y} = t;
+            return u = (0,
+            o.a)({
+                objectFit: b,
+                objectPosition: d,
+                backgroundColor: g
+            }, u),
+            (0,
+            r.uS)(a.createElement(o.L, {
+                layout: f,
+                width: h,
+                height: v
+            }, a.createElement(o.P, (0,
+            o.a)({}, (0,
+            o.g)(p, c, f, h, v, y, b, d))), a.createElement(o.M, (0,
+            o.a)({}, m, {
+                width: h,
+                height: v,
+                className: l
+            }, (0,
+            o.b)(i, c, w, n, u)))))
+        }
+    }
+}]);
+"
