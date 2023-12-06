0c2b49f692103f11eb68db517112e377b1684a60
simonksimon <113455453+simonksimon@users.noreply.github.com>
2023-12-06 09:30:46 +0100

    Add files via upload

---
+++ Warrior.py
@@ -0,0 +1,20 @@
+#https://py.checkio.org/en/mission/the-warriors
+class Warrior:
+    def __init__(self, health=50, attack = 5):
+        self.is_alive=True
+        self.health=health
+        self.attack=attack
+
+class Knight(Warrior):
+    def __init__(self, health=50, attack=5):
+        super.__init__(health,7)
+
+chuck = Warrior()
+bruce = Warrior()
+carl = Knight()
+dave = Warrior()
+
+print(carl.attack)
+
+def fight(w1,w2):
+    pass
