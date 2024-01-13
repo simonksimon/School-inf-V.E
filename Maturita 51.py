a8b56b0ed80e9d66996bd9a04d5ee313e2b32916
simonksimon <113455453+simonksimon@users.noreply.github.com>
2024-01-13 20:01:52 +0100

    Add files via upload

---
+++ Maturita 51.py
@@ -0,0 +1,26 @@
+txt="Cez vikend je planovana odstavka severnej casti linky"
+z=txt.split()
+print("Počet slov vo vete:")
+print(len(z))
+res=""
+for i in range(len(z)):
+    for y in range(len(z[i])):
+        if i%2==0:
+            res=res+z[i][y].upper()
+        else:
+            res=res+z[i][y].lower()
+print(res)
+
+vys=""
+for i in range(len(res)):
+    if res[i].isupper():
+        vys=vys+(res[i].upper())
+        if i!=len(res)-1:
+            if res[i+1].islower():
+                vys=vys+(" ")
+    elif res[i].islower():
+        vys=vys+(res[i].upper())
+        if i!=len(res)-1:
+            if res[i+1].isupper():
+                vys=vys+(" ")
+print(vys)
