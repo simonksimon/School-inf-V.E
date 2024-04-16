32108a75de1985c8b9743e45b7440fa79f264bfd
simonksimon <113455453+simonksimon@users.noreply.github.com>
2024-04-16 08:43:48 +0200

    Delete Maturita 19 (v.).py

--- Maturita 19 (v.).py
+++
@@ -1,9 +0,0 @@
-file=open("preklopenie_obrazka.txt","r")
-f=file.readlines()
-print("Počet pixelov: ",int(int(f[0][0:3]))+int((f[0][4:7])))
-pocetjednotiek=0
-for i in range(len(f)):
-    for y in range(len(f[i])):
-        if f[i][y]=="1":
-            pocetjednotiek+=1
-print("Počet jednotiek: ",pocetjednotiek)
